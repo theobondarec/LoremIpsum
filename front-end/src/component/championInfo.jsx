b7b6a0a3e3df5e33266879989df3ef5e29f2ba5f
Xiangyang <nicolas;te@hotmail.fr>
2020-06-17 17:20:39 +0200

    modifications

--- front-end/src/component/championInfo.jsx
+++ front-end/src/component/championInfo.jsx
@@ -13,49 +13,46 @@ class championInfo extends Component {
             <div className="container" style={{marginTop: 20}}>
                 <div className="row">
                     <div className="col-2 ml-4">
-                        <div class = "card" style={{color: 'd5d5d5', width: 200}}>
-                            <img src={this.state.champions[0].icon} class="rounded mx-auto d-block" alt= {this.state.champions[0].name} style={{width: 120, height: 120, }}/>
+                        <div class = "card" style={{width: 200}}>
+                            <img src={this.state.champions[0].icon} class="rounded mx-auto d-block" alt= {this.state.champions[0].name} style={{marginTop: 40,width: 120, height: 120, }}/>
                             <div class="card-body">
-                                <h1 class="card-title">
+                                <h1 class="card-title" style={{textAlign: 'center'}}>
                                     {this.state.champions[0].name}
                                     <h5>{this.state.champions[0].title}</h5>
                                 </h1>
                             </div>
-                            
                         </div>
                     </div>
-                    <div className="col">
-                        
+                    <div className="col" style={{marginLeft: 40}}>
+                        <main>
+                            <div class="container mt-3">
+                            <table class="table table-dark" >
+                                <thead>
+                                    <tr>
+                                        <th scope="col">HP</th>
+                                        <th scope="col">HP PER LEVEL</th>
+                                        <th scope="col">Armor</th>
+                                        <th scope="col">Movement speed</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">{this.state.champions[0].stats.hp}</th>
+                                        <td>{this.state.champions[0].stats.hpperlevel}</td>
+                                        <td>{this.state.champions[0].stats.armor}</td>
+                                        <td>{this.state.champions[0].stats.movespeed}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            </div>
+                            <div className = "row">
+                                <div className = "col"><a>Placeholder</a></div >
+                                <div className = "col-8"><a>Placeholder for other stats</a></div >
+                                <div className = "col"><a>Placeholder</a></div >
+                            </div>
+                        </main>
                     </div>
                 </div>
-            <main>
-                <div class="container mt-4">
-                <table class="table table-dark" >
-                    <thead>
-                        <tr>
-                            <th scope="col">HP</th>
-                            <th scope="col">HP PER LEVEL</th>
-                            <th scope="col">Armor</th>
-                            <th scope="col">Movement speed</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">{this.state.champions[0].stats.hp}</th>
-                            <td>{this.state.champions[0].stats.hpperlevel}</td>
-                            <td>{this.state.champions[0].stats.armor}</td>
-                            <td>{this.state.champions[0].stats.movespeed}</td>
-                        </tr>
-                    </tbody>
-                
-                </table>
-                </div>
-                <div className = "row">
-                    <div className = "col"><a>Placeholder</a></div >
-                    <div className = "col-8"><a>Placeholder for other stats</a></div >
-                    <div className = "col"><a>Placeholder</a></div >
-                </div>
-            </main>
             </div>
                 
         );
