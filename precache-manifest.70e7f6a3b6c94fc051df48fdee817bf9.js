128f368ef7b66157e0f3b2006cf15162f0b3e4d5
Qu1tus <xavier.bertauld@isen.yncrea.fr>
2020-06-18 16:43:16 +0200

    Updates

---
+++ precache-manifest.70e7f6a3b6c94fc051df48fdee817bf9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753989d553f8c786c79c700af208443d",
+    "url": "/LoremIpsum/index.html"
+  },
+  {
+    "revision": "ab3d82d122cb1926e530",
+    "url": "/LoremIpsum/static/css/2.7b9c8937.chunk.css"
+  },
+  {
+    "revision": "4e764ee46fd2d6ad78e6",
+    "url": "/LoremIpsum/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab3d82d122cb1926e530",
+    "url": "/LoremIpsum/static/js/2.0961a759.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LoremIpsum/static/js/2.0961a759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e764ee46fd2d6ad78e6",
+    "url": "/LoremIpsum/static/js/main.9dab596b.chunk.js"
+  },
+  {
+    "revision": "4040b7217b47df5357f3",
+    "url": "/LoremIpsum/static/js/runtime-main.1a6e2886.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/LoremIpsum/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/LoremIpsum/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/LoremIpsum/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/LoremIpsum/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/LoremIpsum/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
