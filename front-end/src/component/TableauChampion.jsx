af41b2025192c9e2c555852a14a2a67dc2fd0ab8
Xiangyang <nicolas.te@hotmail.fr>
2020-06-18 19:18:09 +0200

    added link

--- front-end/src/component/TableauChampion.jsx
+++ front-end/src/component/TableauChampion.jsx
@@ -1,5 +1,6 @@
 import Champion from './champion';
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class tableauChampion extends Component {
     constructor(props){
