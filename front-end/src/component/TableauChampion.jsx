8a03a60ab1db5d0f9ad4af9c8d9cb5d72f823c40
Courmont <gael.courmont@isen.yncrea.fr>
2020-06-18 19:18:37 +0200

    Tableau champion

--- front-end/src/component/TableauChampion.jsx
+++ front-end/src/component/TableauChampion.jsx
@@ -1,6 +1,7 @@
 import Champion from './champion';
 import React, { Component } from 'react';
-
+import Lol from "./lol.jpg"
+import {Link} from 'react-router-dom'
 class tableauChampion extends Component {
     constructor(props){
         super(props)
@@ -13,29 +14,29 @@ class tableauChampion extends Component {
     handleChange(event){
         this.setState({search:event.target.value})
     }
-
+ 
     render() { 
         return (
             <div className="container-fluid">
                     <nav className="navbar navbar-light bg-light">
-                            <input  className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.state.search} onChange={this.handleChange}/>
                         <form className="form-inline">
-                                search
+                            <input  className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.state.search} onChange={this.handleChange}/>
                             <Link to={"/Champion/"+this.state.search} className="btn btn-outline-success my-2 my-sm-0">
+                                search
                             </Link>
                         </form>
                     </nav>
+
                 <div style={{marginTop: 20}}>
                     <div className="container">
                         <div className="row">
-                                <Champion key={champion.id} champion={champion} />
-                                {this.state.champions.map(champion=><Champion key={champion.id} champion={champion} />)}
+                            {this.state.champions.map(champion=><Champion key={champion.id} champion={champion} />)}
                         </div>
                     </div>
                 </div>
             </div>
-        );
+         );
     }
 }
-
+ 
 export default tableauChampion;
