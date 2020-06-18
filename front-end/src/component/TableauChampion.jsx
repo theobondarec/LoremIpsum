c1d6885a29a8132e8d26fc27c01988d27a337d2e
Courmont <gael.courmont@isen.yncrea.fr>
2020-06-18 19:01:34 +0200

    Search bar fix

--- front-end/src/component/TableauChampion.jsx
+++ front-end/src/component/TableauChampion.jsx
@@ -1,20 +1,31 @@
 import Champion from './champion';
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 class tableauChampion extends Component {
-    state = {
-        champions:require('./champ.json')
+    constructor(props){
+        super(props)
+    this.handleChange=this.handleChange.bind(this);
     }
+    state = {
+        champions:require('./champ.json'),
+        search:""
+    }
+    handleChange(event){
+        this.setState({search:event.target.value})
+    }
+ 
     render() { 
         return (
             <div className="container-fluid">
-                <nav className="navbar navbar-light bg-light">
-                    <form className="form-inline">
-                        <input  className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.state.search} onChange={this.handleChange}/>
-                        <Link to={"/Champion/"+this.state.search} className="btn btn-outline-success my-2 my-sm-0">
-                            search
-                        </Link>
-                    </form>
-                </nav>
+                    <nav className="navbar navbar-light bg-light">
+                        <form className="form-inline">
+                            <input  className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.state.search} onChange={this.handleChange}/>
+                            <Link to={"/Champion/"+this.state.search} className="btn btn-outline-success my-2 my-sm-0">
+                                search
+                            </Link>
+                        </form>
+                    </nav>
+
                 <div style={{marginTop: 20}}>
                     <div className="container">
                         <div className="row">
@@ -24,8 +35,8 @@ class tableauChampion extends Component {
                     </div>
                 </div>
             </div>
-        );
+         );
     }
 }
-
+ 
 export default tableauChampion;
