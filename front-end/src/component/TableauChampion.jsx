91793553c82db283725f1cb07ab64ea2955f01a1
Xiangyang <nicolas.te@hotmail.fr>
2020-06-18 20:10:16 +0200

    added bootstrap

--- front-end/src/component/TableauChampion.jsx
+++ front-end/src/component/TableauChampion.jsx
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import Champion from './champion';
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-
-class tableauChampion extends Component {
-    constructor(props){
-        super(props)
-    this.handleChange=this.handleChange.bind(this);
-    }
-    state = {
-        champions:require('./champ.json'),
-        search:""
-    }
-    handleChange(event){
-        this.setState({search:event.target.value})
-    }
-
-    render() { 
-        return (
-            <div className="container-fluid">
-                    <nav className="navbar navbar-light bg-light">
-                            <input  className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.state.search} onChange={this.handleChange}/>
-                        <form className="form-inline">
-                                search
-                            <Link to={"/Champion/"+this.state.search} className="btn btn-outline-success my-2 my-sm-0">
-                            </Link>
-                        </form>
-                    </nav>
-                <div style={{marginTop: 20}}>
-                    <div className="container">
-                        <div className="row">
-                                <Champion key={champion.id} champion={champion} />
-                                {this.state.champions.map(champion=><Champion key={champion.id} champion={champion} />)}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-=======
 import Champion from './champion';
 import React, { Component } from 'react';
 import Lol from "./lol.jpg"
@@ -57,7 +14,7 @@ class tableauChampion extends Component {
     handleChange(event){
         this.setState({search:event.target.value})
     }
- 
+
     render() { 
         return (
             <div className="container-fluid">
@@ -69,7 +26,6 @@ class tableauChampion extends Component {
                             </Link>
                         </form>
                     </nav>
-
                 <div style={{marginTop: 20}}>
                     <div className="container">
                         <div className="row">
@@ -78,9 +34,8 @@ class tableauChampion extends Component {
                     </div>
                 </div>
             </div>
-         );
+        );
     }
 }
- 
->>>>>>> FRONT-end-vf
+
 export default tableauChampion;
