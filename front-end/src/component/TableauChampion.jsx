d3fd6e839b2039afa02dc384a8224ccd8f73b4db
Courmont <gael.courmont@isen.yncrea.fr>
2020-06-18 18:51:28 +0200

    import axios et navbar champion

--- front-end/src/component/TableauChampion.jsx
+++ front-end/src/component/TableauChampion.jsx
@@ -1,16 +1,28 @@
 import Champion from './champion';
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+
 class tableauChampion extends Component {
     state = {
         champions:require('./champ.json')
     }
     render() { 
         return (
-            <div style={{marginTop: 20}}>
-                <div className="container">
-                    <div className="row">
-                        {this.state.champions.map(champion=>
-                            <Champion key={champion.id} champion={champion} />)}
+            <div className="container-fluid">
+                <nav className="navbar navbar-light bg-light">
+                    <form className="form-inline">
+                        <input  className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" value={this.state.search} onChange={this.handleChange}/>
+                        <Link to={"/Champion/"+this.state.search} className="btn btn-outline-success my-2 my-sm-0">
+                            search
+                        </Link>
+                    </form>
+                </nav>
+                <div style={{marginTop: 20}}>
+                    <div className="container">
+                        <div className="row">
+                            {this.state.champions.map(champion=>
+                                <Champion key={champion.id} champion={champion} />)}
+                        </div>
                     </div>
                 </div>
             </div>
