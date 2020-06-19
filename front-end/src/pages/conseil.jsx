3ae8c2285e170f8a1188f2901fad0f96b95c3834
Xiangyang <nicolas.te@hotmail.fr>
2020-06-19 18:15:19 +0200

    added space in conseils.jsx

--- front-end/src/pages/conseil.jsx
+++ front-end/src/pages/conseil.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import conseil_joueur from './conseil_joueur';
+//import conseil_joueur from './conseil_joueur';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 
@@ -7,26 +7,26 @@ import {Link} from 'react-router-dom';
 class Conseil extends Component {
     constructor(props){
         super(props)
-    this.handleChangePseudo=this.handleChangePseudo.bind(this);
-    this.handleChangeChampion=this.handleChangeChampion.bind(this)
-    this.handleClick=this.handleClick.bind(this);
+    this.handleChangePseudo = this.handleChangePseudo.bind(this);
+    this.handleChangeChampion = this.handleChangeChampion.bind(this)
+    this.handleClick = this.handleClick.bind(this);
     }
     state = { 
-        pseudo:"",
-        champion:"",
-        infos:{}
+        pseudo: "",
+        champion: "",
+        infos: {}
     }
-    handleChangeChampion(event){
-        this.setState({champion:event.target.value})
+    handleChangeChampion(event) {
+        this.setState ({ champion:event.target.value })
     }
-    handleChangePseudo(event){
-        this.setState({pseudo:event.target.value})
+    handleChangePseudo (event) {
+        this.setState({ pseudo:event.target.value })
     }
 
-    handleClick(event){
+    handleClick(event) {
         event.preventDefault();
         //axios.get("http://localhost:8080/Conseil/"+this.state.pseudo+"/"+this.state.champion).then(response=>{this.setState({infos:response})})
-        axios.get("http://localhost:8080/Conseil/"+this.state.pseudo+"/"+this.state.champion).then(function(response){console.log(response)})
+        axios.get("http://localhost:8080/Conseil/" + this.state.pseudo + "/" + this.state.champion).then(function(response){ console.log(response) })
         console.log(this.state.infos)
     }
 
@@ -34,11 +34,11 @@ class Conseil extends Component {
         return (
             <div>
                 <div >
-                    <nav className="navbar navbar-light bg-light">
-                        <form className="form-inline" onSubmit={this.handleClick}>
-                            <input  className="form-control mr-sm-2" type="pseudo" placeholder="Pseudo" aria-label="Pseudo" value={this.state.pseudo} onChange={this.handleChangePseudo}/>
-                            <input  className="form-control mr-sm-2" type="champion" placeholder="champion" aria-label="Pseudo" value={this.state.champion} onChange={this.handleChangeChampion}/>
-                            <Link to={"/Conseil/"+this.state.pseudo+"/"+this.state.champion}>
+                    <nav className = "navbar navbar-light bg-light">
+                        <form className = "form-inline" onSubmit = { this.handleClick }>
+                            <input  className = "form-control mr-sm-2" type = "pseudo" placeholder = "Pseudo" aria-label = "pseudo" value = { this.state.pseudo } onChange = { this.handleChangePseudo }/>
+                            <input  className = "form-control mr-sm-2" type = "champion" placeholder = "champion" aria-label = "champion" value = { this.state.champion } onChange = { this.handleChangeChampion }/>
+                            <Link to = { "/Conseil/" + this.state.pseudo + "/" + this.state.champion }>
                                 Search
                             </Link>
                         </form>
