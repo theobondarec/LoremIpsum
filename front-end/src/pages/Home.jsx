9b9de9fb9689acdf0c6c4e38e607c6913213115b
Xiangyang <nicolas.te@hotmail.fr>
2020-06-19 10:17:13 +0200

    removed bootstrap components

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,46 +1,25 @@
 import React, { Component } from 'react';
-import Lol from '../component/lol.jpg';
 import axios from 'axios';
 
 
-class Home extends Component {
+export default class Home extends Component {
     constructor (props){
         super(props)
     
         this.state = { 
             conseil:axios.get("http://localhost:8080/").then(response=>this.setState({conseil:response.data}))
         }
-        this.styles={
-            background:{
-            backgroundImage: "url("+Lol+")", 
-            width:"100%",
-            height:"400px%",
-            backgroundSize: "cover",
-            nackgroundPosition: "center",
-        }
-        }
-    this.handleClick=this.handleClick.bind(this)
-    };
+    }
+    this.handleClick=this.handleClick.bind(this);
     handleClick(event){
         console.log(this.state.conseil)
     }
     render() { 
         return (
-            <body style={this.styles.background} >
-                <div className="container"  >
-                    <h1 style={{color:"Red"}}>
-                        HOME PAGE
-                    </h1>
-                    <p style={{color:"black"}}>
-                        Welcome to ISEN.gg the fastest way to reach challenger
-                    </p>
-                    <button onClick={this.handleClick}>
-                        CLICKKKKKKKKKK
-                    </button>
-                </div>
-            </body>
-         );
+            <div className="container">
+                <h1 style={{color:"Red"}}>HOME PAGE</h1>
+                <p style={{color:"black"}}>Welcome to ISEN.gg the fastest way to reach challenger</p>
+            </div>
+        );
     }
 }
- 
-export default Home;
