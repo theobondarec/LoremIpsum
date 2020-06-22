3b8b37065547ee926d40809f2085fd8e9eb87534
theobondarec <47282766+theobondarec@users.noreply.github.com>
2020-06-22 19:56:39 +0200

    rename FrontEnd to Website

---
+++ webSite/src/pages/conseil.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import conseil_joueur from './conseil_joueur';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+
+class Conseil extends Component {
+    constructor(props){
+        super(props)
+    this.handleChangePseudo=this.handleChangePseudo.bind(this);
+    this.handleChangeChampion=this.handleChangeChampion.bind(this)
+    }
+    state = { 
+        pseudo:"",
+        champion:"",
+        champions:require('../assets/champ.json')
+    }
+    handleChangeChampion(event){
+        //barre de recherche champion
+        this.setState({champion:event.target.value})
+    }
+    handleChangePseudo(event){
+        //barre de recherche Pseudo
+        this.setState({pseudo:event.target.value})
+    }
+    
+
+
+    render() { 
+        return (
+            <div>
+                <div >
+                    <nav className="navbar navbar-light bg-light">
+                        <form className="form-inline" >
+                            <input  className="form-control mr-sm-2" type="pseudo" placeholder="Pseudo" aria-label="Pseudo" value={this.state.pseudo} onChange={this.handleChangePseudo}/>
+                            <input  className="form-control mr-sm-2" type="champion" placeholder="champion" aria-label="Pseudo" value={this.state.champion} onChange={this.handleChangeChampion}/>
+                            <Link to={"/Conseil/"+this.state.pseudo+"/"+this.state.champion}>
+                                Search
+                            </Link>
+                        </form>
+                    </nav>
+                </div>
+            </div>
+            );
+    }
+}
+
+export default Conseil;
