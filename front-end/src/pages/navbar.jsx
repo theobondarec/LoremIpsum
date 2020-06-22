eb74ca4bde0835667a698a3fa0063a8fa7263bff
theobondarec <47282766+theobondarec@users.noreply.github.com>
2020-06-22 16:51:15 +0200

    Add files via upload

---
+++ front-end/src/pages/navbar.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import Logo from "../assets/Logo.jpg"
+
+class NavBar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <h3>
+                <img src={Logo} alt="logo" width="40% \9" />
+            </h3>
+            <ul>
+                <Link to='/'>
+                    <li className="btn btn-outline-success">Home</li>
+                </Link>
+                <Link to='/Champion'>
+                    <li className="btn btn-outline-secondary">Page Champions</li>
+                </Link>
+                <Link to='/Profile'>
+                    <li className="btn btn-outline-secondary">Profile</li>
+                </Link >
+                <Link to='/Conseil'>
+                    <li className="btn btn-outline-secondary">Conseil</li>
+                </Link>
+            </ul>
+        </nav>
+            );
+    }
+}
+
+export default NavBar;
